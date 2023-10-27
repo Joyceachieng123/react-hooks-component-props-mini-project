bad6a5396492246736cd9b0ed87702b8c8cf3d77
Joyce Achieng <joyce.achieng@student.moringaschool.com>
2023-10-27 23:06:10 +0300

    Complete Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,51 @@
 import React from "react";
 import blogData from "../data/blog";
-
+import Header from "./Header";
+import ArticleList from "./Article list";
+import About from "./About";
 console.log(blogData);
 
+
 function App() {
+  const about = "Personal blog by Dan Abramv.I explain with words and codes.";
+  const articles =[
+    {
+      id: 1,
+      title: "The WET code base",
+      date: "October 15, 2023",
+      preview: "Come waste your Time with me",
+      minutesToRead: "1 min read",
+    },
+    {
+      id: 2,
+      title: "Goodbye, cleancode",
+      date: "January 11, 2020",
+      preview: "Let clean code guide you. Then let it go",
+      minutesToRead: "5 min Read"
+    },
+    {
+    id: 3,
+      title: "My Decade in Review",
+      date: "January 1, 2020",
+      preview: "A personal reflection",
+      minutesToRead: "26 min Read"
+    }
+  ];
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+     
+    <Header blog= "Overreacted"/>
+    <About
+        image="https://via.placeholder.com/215"
+        aboutText={about}
+      />
+      <ArticleList articles={articles} />
     </div>
   );
 }
+
+  
+  
+
 
 export default App;
---
+++ src/components/Article list.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({ posts }) {
+    
+  return (
+<main>
+    
+        {posts && 
+      posts.map((post,index) => (
+        <Article
+          key={index}
+          title={post.title}
+          date={post.date || "January 1, 1970"}
+          preview={post.preview}
+          
+        />
+      ))}
+    
+    </main>
+  );
+}
+
+export default ArticleList;
